0edf5c98d99dd140cb25174dc6fb267e4653f3e4
Barri <info@bdesignstudio.co.uk>
2020-07-27 21:33:40 +0100

    Set up call back function

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,15 +1,31 @@
 import React, { Component } from 'react'
 
 export class SearchBar extends Component {
+
+  state = { userInput: '' };
+
+  onInputChange = (e) => {
+
+    this.setState({ userInput: e.target.value })
+  };
+
+  onFormSubmit=(e)=>{
+    e.preventDefault();
+
+   //we call call back from parent component 
+
+  };
   render() {
     return (
       <div className="search-bar ui segment">
-       <form className="ui form"onSubmit ="">
-         <div className="field">
-         <label className="ui label" htmlFor="">Video Search</label>
-         <input className="ui input" type="text"/>
-         </div>
-       </form>
+        <form className="ui form" onSubmit={this.onFormSubmit}>
+          <div className="field">
+            <label className="ui label" htmlFor="">Video Search</label>
+            <input className="ui input" type="text" value={this.state.userInput}
+              onChange={this.onInputChange}
+            />
+          </div>
+        </form>
       </div>
     )
   }
