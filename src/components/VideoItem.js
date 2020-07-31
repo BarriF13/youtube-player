9e727f3b8b5e6374747ae299d2f228bf4ce503ae
Barri <info@bdesignstudio.co.uk>
2020-07-31 12:09:16 +0100

    Styleing

--- src/components/VideoItem.js
+++ src/components/VideoItem.js
@@ -2,7 +2,7 @@ import React from 'react'
 import  './videoItem.css'
 const VideoItem = ({ video }) => {
   return (
-    <div className="item">
+    <div className="video-item item">
       <img className="ui image" src={video.snippet.thumbnails.medium.url} />
       <div className=" content ">
         <div className="header">
